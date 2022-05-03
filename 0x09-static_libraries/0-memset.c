1b8d82e5ce8825fa2eab8ebd03b87d7789ed51eb
Sephens <adesephens@gmail.com>
2022-05-03 14:51:56 -0700

    Made alot of corrections

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,6 @@
+/* Filename: lib_mylib.c */
+#include <stdio.h>
+void fun(void)
+{
+	char *_memset(char *s, char b, unsigned int n);
+}
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,6 @@
+/* Filename: lib_mylib.c */
+#include <stdio.h>
+void fun(void)
+{
+	char *_memcpy(char *dest, char *src, unsigned int n);
+}
