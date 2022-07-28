fcec86f07fa4553f80dfed7c43c0c030886c4d41
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-07-29 02:44:57 +0300

    Update 0-main.c

--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -3,11 +3,6 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
- */
 int main(void)
 {
 	hash_table_t *ht;
